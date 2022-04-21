85232785445d2f3f20a41d93ad6151e649e62071
Palash Mandal <pmpalashmandal@hmail.com>
2022-04-21 21:37:48 +0600

    update netlify

---
+++ src/pages/index.tsx
@@ -0,0 +1,61 @@
+import type { NextPage } from 'next'
+import { useState } from 'react'
+
+const Home: NextPage = () => {
+  type userInfoType = {
+    fullname: string;
+    id: string | number;
+    section?: string;
+    phone: number;
+    email: string;
+}
+const [userInfo, setUserInfo] = useState<userInfoType>({
+  fullname: "",
+  id: "",
+  section: "",
+  phone: 0,
+  email: ""
+})
+
+const handelChange = (e) => {
+  const {name, value} = e.target;
+  setUserInfo({...userInfo, [name]: value})
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(userInfo.fullname);
+  console.log(userInfo.id);
+  console.log(userInfo.phone);
+  console.log(userInfo.email);
+}
+
+  return (
+    <div className='w-25 mx-auto'>
+
+        <form className='m-4' id="my_form" onSubmit={handleSubmit} >
+                  <div className="mb-3">
+                    <label className="form-label">Name</label>
+                    <input type="text" name="fullname" className="form-control" value={userInfo.fullname} onChange={handelChange}  />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">ID</label>
+                    <input type="text" name="id" className="form-control" value={userInfo.id} onChange={handelChange}  />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Phone</label>
+                    <input type="number" name="phone" className="form-control" value={userInfo.phone} onChange={handelChange} />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Email</label>
+                    <input type="email" name="email" className="form-control" value={userInfo.email} onChange={handelChange}  />
+                  </div>
+                  
+                  <button type="submit" className="btn btn-primary">Submit</button>
+              </form>
+    </div>
+      
+  )
+}
+
+export default Home
